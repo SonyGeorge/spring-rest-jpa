951d9601bfab839aa8065234c7647d1d37aedab1
Sony George <mr.sony.george@gmail.com>
2022-05-09 18:47:42 +0900

    Update V1_2002__new_data.sql

--- src/main/resources/db/migration/V1_2002__new_data.sql
+++ src/main/resources/db/migration/V1_2002__new_data.sql
@@ -1,1 +1,7 @@
-insert
+INSERT INTO `transactions` (`coin_name`, `amount`, `date_time`) VALUES ('BTC', 56.124000, '2022-02-02 01:02:55');
+INSERT INTO `transactions` (`coin_name`, `amount`, `date_time`) VALUES ('BTC', 34.510000, '2022-02-03 02:04:07');
+INSERT INTO `transactions` (`coin_name`, `amount`, `date_time`) VALUES ('BTC', 67.485000, '2022-02-04 03:06:19');
+INSERT INTO `transactions` (`coin_name`, `amount`, `date_time`) VALUES ('BTC', 12.412000, '2022-02-04 04:08:33');
+INSERT INTO `transactions` (`coin_name`, `amount`, `date_time`) VALUES ('BTC', 43.124000, '2022-02-05 05:10:55');
+INSERT INTO `transactions` (`coin_name`, `amount`, `date_time`) VALUES ('BTC', 23.510000, '2022-02-05 06:12:07');
+INSERT INTO `transactions` (`coin_name`, `amount`, `date_time`) VALUES ('BTC', 65.485000, '2022-02-05 07:14:19');
